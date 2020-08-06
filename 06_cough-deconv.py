c9b1b2cf7b6dd1eb44f447b8c86d081ed90e35bb
Andreas Karg <andreas.karg@bluefruit.co.uk>
2020-08-06 13:43:23 +0000

    Try with cough / brief thresholding experiment

--- 06_cough-deconv.py
+++ 06_cough-deconv.py
@@ -152,10 +152,10 @@ deconv_layers = create_deconv(model, LAYER_NAME)
 
 deconv_layers
 
-s = extract_feature(path.join(recordings_dir,"dog.wav"))
+s = extract_feature(path.join(recordings_dir,"coughing.wav"))
 show_image(s[0,:,:,0],'magma')
 
-output = forward_pass(deconv_layers, extract_feature(path.join(recordings_dir,"dog.wav")))
+output = forward_pass(deconv_layers, extract_feature(path.join(recordings_dir,"coughing.wav")))
 
 result = backward_pass(deconv_layers, output, 46, 'all')
 show_image(result)
@@ -178,7 +178,7 @@ multiplot(max_act[:32],'max', cols=4)
 LAYER_NAME = 'block5_pool'
 deconv_layers = create_deconv(model, LAYER_NAME)
 
-output = forward_pass(deconv_layers, extract_feature(path.join(recordings_dir,"dog.wav")))
+output = forward_pass(deconv_layers, extract_feature(path.join(recordings_dir,"coughing.wav")))
 
 multiplot(range(32), cols=4)
 
@@ -187,114 +187,14 @@ multiplot(range(32), cols=4)
 max_act = max_activation(output)
 multiplot(max_act[:32], cols=4)
 
-# Most of the highly activated filters seem to be centering on the bark, but they have at least some of the silence included
-
-multiplot(max_act[160:164], cols=4)
-
-# About 150 of 512 layers are being activated for dog.wav
-
 multiplot(max_act[:32],'max', cols=4)
 
 # At this layer, the 'max' and 'all' look pretty similar
 
 #Show most activated filter full size
-result = backward_pass(deconv_layers, output, 267, 'all')
+result = backward_pass(deconv_layers, output, 347, 'all')
 show_image(result)
 
 show_image(result,'magma')
-
-# # Conclusions
-# Although interesting, this doesn't seem to give any clues as to why dog.wav is categorised as a sneeze. The large number of edges being detected in the early layers in the silence portion may be a problem, but later layers seem to filter most of it out.
-#
-# Deconvnet.py is still quite hacky. Current issues include:
-# - DPooling is really slow for forward pass. Probably due to python for loops rather than a proper tensor math solution
-# - DDense has not been used, so is untested.
-# - Will probably break for TF2.3
-
-# ### Check inference results for samples
-
-# +
-try:
-    import tflite_runtime.interpreter as tflite
-except ModuleNotFoundError:
-    import tensorflow.lite as tflite
-
-def get_human_readable_output(output):
-    results = [
-        'breathing', 'car_horn', 'cat',
-        'chainsaw', 'chirping_birds',
-        'clapping', 'coughing', 'cow',
-        'crickets', 'dog', 'door_wood_knock',
-        'engine', 'frog', 'glass_breaking',
-        'hen', 'laughing', 'rain', 'rooster',
-        'sheep', 'siren', 'sneezing',
-        'snoring', 'thunderstorm',
-        'vacuum_cleaner', 'wind'
-    ]
-
-    output = list(output[0])
-
-    largest = max(output)
-    index = list(output).index(largest)
-    #return results[index]
-    result = f"{largest * 100:.2f}% confident that this is a {results[index]}"
-
-    formatted_output = [f"{o*100:05.2f}" for o in output]
-    stats = [f"{x}%: {results[i]}" for i, x in enumerate(formatted_output)]
-
-    return result, "\n".join(stats)
-
-
-def infer_from_file(filename, model_path="/home/jupyter/dev/tflow/saved-models/esc_25_vgg16-03-08-20-14-52.tflite"):
-    interpreter = tflite.Interpreter(model_path=model_path)
-    interpreter.allocate_tensors()
-
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
-    input_data = extract_feature(filename)
-
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.invoke()
-    predicted_data_vector = interpreter.get_tensor(output_details[0]['index'])
-
-    return get_human_readable_output(predicted_data_vector)
-
-
-# -
-
-infer_from_file(path.join(recordings_dir,"coughing.wav"))
-
-infer_from_file(path.join(recordings_dir,"dog.wav"))
-
-infer_from_file(path.join(recordings_dir,"vacuum_cleaner.wav"))
-
-
-# ## Thresholding
-
-# +
-
-def extract_feature(filename):
-    y, sr = librosa.load(filename, sr=44100) 
-    min = y.min() * 0.5
-    y[y<min] = min
-    S = librosa.feature.melspectrogram(
-        y=y,
-        sr=sr,
-        fmax=(sr / 2.0),
-        fmin=0.0,
-        n_fft=2048,
-        hop_length=512,
-        n_mels=128
-    )
-    S = librosa.power_to_db(S)
-    S = np.transpose(S)
-    return S[np.newaxis, 0:431, :, np.newaxis]
-
-
-# -
-
-infer_from_file(path.join(recordings_dir,"dog.wav"))
-
-# Thresholding is actually giving a worse prediction - but it does confirm that it is the "silence" that is convincing the model that this is a sneeze. Maybe try re-training the model with thresholding and a silence category?
 
 
